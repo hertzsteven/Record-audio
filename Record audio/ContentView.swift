5d749a1bdae680750beb5af758031fa8f356b1b7
Steven Hertz <StevenH@appi4.com>
2025-09-05 10:42:49 -0400

    Initial Commit

---
+++ Record audio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Record audio
+//
+//  Created by Steven Hertz on 9/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Record audio/Record_audioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_audioApp.swift
+//  Record audio
+//
+//  Created by Steven Hertz on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Record_audioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
